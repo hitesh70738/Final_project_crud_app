73a4f896d9a6fc673b02fdfb175c295b0d432561
hitesh70738 <hitesh70738@gmail.com>
2021-01-02 22:43:31 +0000

    Jenkins

---
+++ tests/test_int.py
@@ -0,0 +1,70 @@
+import unittest
+import time
+from flask import url_for
+from urllib.request import urlopen
+
+from flask_testing import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from application import app, db
+from application.models import Teams, Players
+
+class TestBase(LiveServerTestCase):
+
+    def create_app(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:1234@34.105.208.208/crud_app"
+        app.config['SECRET_KEY'] = "asdasda"
+        return app
+
+    def setUp(self):
+        """Setup the test driver and create test users"""
+        print("--------------------------NEXT-TEST----------------------------------------------")
+        chrome_options = Options()
+        chrome_options.binary_location = "/usr/bin/chromium-browser"
+        chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(executable_path="/home/hites/chromedriver", chrome_options=chrome_options)
+        self.driver.get("http://localhost:5000")
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+
+    def tearDown(self):
+        self.driver.quit()
+        print("--------------------------END-OF-TEST----------------------------------------------\n\n\n-------------------------UNIT-AND-SELENIUM-TESTS----------------------------------------------")
+
+    def test_server_is_up_and_running(self):
+        response = urlopen("http://localhost:5000")
+        self.assertEqual(response.code, 200)
+
+class TestCreateTeam(TestBase):
+
+    def create_team(self):
+        self.driver.find_element_by_xpath("/html/body/a[2]").click()
+        time.sleep(1)
+
+        self.driver.find_element_by_xpath('//*[@id="team_name"]').send_keys('Arsenal')
+        self.driver.find_element_by_xpath('//*[@id="sponsor"]').send_keys('Emirates')
+        self.driver.find_element_by_xpath('//*[@id="submit"]').click()
+        time.sleep(1)
+
+        assert url_for('home') in self.driver.current_url
+        assert Teams.query.filter_by(id=1).first().team_name == 'Arsenal'
+        assert Teams.query.filter_by(id=1).first().sponsor == 'Emirates'
+
+class TestAddPlayer(TestCreateTeam):
+    def add_player(self):
+        self.driver.find_element_by_xpath('/html/body/form[3]/input').click()
+        time.sleep(1)
+
+        self.driver.find_element_by_xpath('//*[@id="name"]').send_keys('Thomas Partey')
+        self.driver.find_element_by_xpath('//*[@id="position"]').send_keys('CDM')
+        self.driver.find_element_by_xpath('//*[@id="submit"]').click()
+        time.sleep(1)
+
+        assert url_for('add') in self.driver.current_url
+        assert Players.query.filter_by(id=1).first().name == 'Thomas Partey'
+        assert Players.query.filter_by(id=1).first().position == 'CDM'
+
+
+if __name__ == '__main__':
+    unittesting.main(port=5000)
---
+++ tests/test_unit.py
@@ -0,0 +1,81 @@
+import unittest
+from flask import url_for
+from flask_testing import TestCase
+from application import app, db
+from application.models import Teams, Players
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:1234@34.105.208.208/crud_app",
+            SECRET_KEY='TEST_SECRET_KEY',
+            DEBUG=True
+        )
+        return app
+
+    def setUp(self):
+        db.create_all() 
+        test_team = Teams(team_name="team name", sponsor="Emirates")
+        db.session.add(test_team)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+class TestViews(TestBase):
+    def test_home_get(self):
+        response = self.client.get(url_for("home"))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_create_get(self):
+        response = self.client.get(url_for('create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_get(self):
+        response = self.client.get(url_for('update', id=1), follow_redirects=True)
+        self.assertEqual(response.status_code,200)
+
+    def test_add_get(self):
+        response = self.client.get(url_for('add', id=1), follow_redirects=True)
+        self.assertEqual(response.status_code,200)
+        
+    def test_delete_get(self):
+        response = self.client.get(url_for('delete', id=1), follow_redirects=True)
+        self.assertEqual(response.status_code,200)
+
+class TestRead(TestBase):
+    def test_read_tasks(self):
+        response = self.client.get(url_for("home"))
+        self.assertIn(b"team name", response.data)
+
+class TestCreate(TestBase):
+    def test_create_task(self):
+        response = self.client.post(url_for("create"),
+            data=dict(team_name="New team", sponsor="Emirates"),
+            follow_redirects=True
+        )
+        self.assertIn(b"New team", response.data)
+
+class TestUpdate(TestBase):
+    def test_update_task(self):
+        response = self.client.post(url_for("update", id=1),
+            data=dict(team_name="Updated name", sponsor="updated sponsor"),
+            follow_redirects=True
+        )
+        self.assertIn(b"Updated name", response.data)
+
+class TestAdd(TestBase):
+    def test_add_task(self):
+        response = self.client.post(url_for("add", id=1),
+            data=dict(name="add name", position='CM'),
+            follow_redirects=True
+        )
+        self.assertIn(b"add name", response.data)
+
+class TestDelete(TestBase):
+    def test_update_task(self):
+        response = self.client.get(url_for("delete", id=1),
+            follow_redirects=True
+        )
+        self.assertNotIn(b"team name", response.data)
+        self.assertNotIn(b"Emirates", response.data)
