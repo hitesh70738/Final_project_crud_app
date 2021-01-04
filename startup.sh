93c72ec26c2e33a8325c0092fcad71d0905f9cc4
hitesh70738 <hitesh70738@gmail.com>
2021-01-04 17:03:34 +0000

    startup.sh updated

--- startup.sh
+++ startup.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
-cd /opt/crud_app
-sudo python3 -m venv venv
+
+sudo apt-get update
+sudo apt-get install python3 -y
+sudo apt-get install python3-venv -y
+sudo apt-get install python3-pip -y
+
+
+# Test Phase
+python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+
+
+# pytest goes here
+python3 -m pytest tests/test_unit.py --cov=application
+
+# Deploy Phase
+python3 create.py
 python3 app.py
+
+
+# Make the installation directory
+
+
+
+
+# Give jenkins user permissions for the installation directory
+sudo chown -R jenkins /opt/crud_app
+
+
+sudo systemctl daemon-reload
+sudo systemctl stop crud_app.service
+sudo systemctl start crud_app.service
